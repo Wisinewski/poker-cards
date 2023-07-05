ba2a52a1d97bdbefe59b343cd17dd2220e8b6e72
Wisinewski <g_gomees@hotmail.com>
2023-07-05 01:10:04 -0300

    feat: implement deck with its features

---
+++ deck.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
+)
+
+type deck []string
+
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return os.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func newDeck() deck {
+	cards := deck{}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+
+	for _, value := range cardValues {
+		for _, suit := range cardSuits {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, err := os.ReadFile(filename)
+
+	if err != nil {
+		fmt.Println(":: error:", err)
+		os.Exit(1)
+	}
+
+	return deck(strings.Split(string(bs), ","))
+}
+
+func (d deck) shuffle() {
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+
+	for i := range d {
+		randomIndex := r.Intn(len(d) - 1)
+		d[i], d[randomIndex] = d[randomIndex], d[i]
+	}
+}
--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
 func main() {
+	filename := "my_cards"
 
+	cards := newDeck()
+	cards.shuffle()
+
+	hand, _ := deal(cards, 5)
+	hand.print()
+
+	hand.saveToFile(filename)
+	hand = newDeckFromFile(filename)
+
+	hand.print()
 }
